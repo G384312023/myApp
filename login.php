90ce5cdc962ba5df5f7aec720cd7a10670324e3b
G384312023 <238431@st.takushoku-u.ac.jp>
2025-07-27 12:43:30 +0900

    login.phpにメールアドレス, パスワードフォームを作成

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン</title>
+</head>
+<body>
+    <form action = '' method = 'post'>
+        <input type = 'email' name = 'email' placeholder = 'メールアドレス'><br>
+        <input type = 'password' name = 'password' placeholder = 'パスワード'><br>
+        <input type = 'submit' name = 'submit' placeholder = 'ログイン'>
+    </form>
+</body>
+</html>
