3204c2d3cf6ca5f6427dc3d1bacb79ab0661f6bb
G384312023 <238431@st.takushoku-u.ac.jp>
2025-08-02 11:43:02 +0900

    ログイン・登録微修正

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-  echo "toDoリストにようこそ";
+  session_start();
+
+  if(!isset($_SESSION['user'])) {
+    header('location: Login.php');
+    exit;
+  }
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Todo</title>
+</head>
+<body>
+  <h2><?php echo htmlspecialchars($_SESSION['user']['username']); ?>さん！</h2>
+  <p><a href = "Logout.php">ログアウト</a></p>
+</body>
+</html>
