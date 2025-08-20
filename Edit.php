8936162f5f89cb24b0231c9639853b88c677d497
G384312023 <238431@st.takushoku-u.ac.jp>
2025-08-20 15:38:58 +0900

    chore: 微修正

--- Edit.php
+++ Edit.php
@@ -68,8 +68,8 @@
         <?php endif; ?>
 
         <form action = "" method = "post">
-        <input type = "text" name = "title" placeholder = "タスク名" value = "<?= htmlspecialchars($title ?? '')?>"><br>
-        <textarea name = "description" placeholder = "タスク詳細（Markdown記法対応）&#10;# 見出し1 ## 見出し2 ### 見出し3&#10;**太字** *斜体*&#10;- リスト項目&#10;[リンクテキスト](URL)&#10;`コード`"><?= htmlspecialchars($task['description'] ?? '') ?></textarea><br>
+        <input type = "text" name = "title" placeholder = "タスク名" value = "<?= htmlspecialchars($_POST['title'] ?? $task['title'] ?? '')?>"><br>
+        <textarea name = "description" placeholder = "タスク詳細（Markdown記法対応）&#10;# 見出し1 ## 見出し2 ### 見出し3&#10;**太字** *斜体*&#10;- リスト項目&#10;[リンクテキスト](URL)&#10;`コード`"><?= htmlspecialchars($_POST['description'] ?? $task['description'] ?? '') ?></textarea><br>
         <button type = "submit">編集</button>
         </form>
     </main>
